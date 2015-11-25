b950113ffe6ed7389cd9887d4e1cdc361ad3957f
Colin Surprenant <colin.surprenant@gmail.com>
2015-11-25 02:09:21 -0500

    fix fieldref nil value assignment

--- logstash-core-event-java/spec/event_spec.rb
+++ logstash-core-event-java/spec/event_spec.rb
@@ -90,6 +90,12 @@ describe LogStash::Event do
       expect(e["[foo][2]"]).to eq(1.0)
       expect(e["[foo][3]"]).to be_nil
     end
+
+    it "should set nil value" do
+      e = LogStash::Event.new()
+      e["[foo]"] = nil
+      expect(e.to_hash).to include("foo")
+    end
   end
 
   context "timestamp" do
